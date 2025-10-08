9a73e05c4da39d255722c6150fc9c1b60309ffff
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-08 14:26:21 +0500

    New commit

---
+++ variables.py
@@ -0,0 +1,28 @@
+
+name = "Ania Forger"
+
+age = 6.6 
+
+grade = 1 
+
+Female = True 
+
+print(name)
+print("Name type: ", type(name))
+
+print(age)
+print("Age type: ", type(age))
+
+print(grade)
+print("Grade type: ", type(grade))
+
+print(Female)
+print("Female type: ", type(Female))
+
+#we can change 'em as well 
+
+age = 7 
+print("Modified age: ", age , "years")
+
+grade = 2 
+print("Modified grade: ", grade )
