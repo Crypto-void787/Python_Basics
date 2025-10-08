50907cd759d0fb6cdc37723a831ef70ad56d98f3
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-08 09:55:27 +0500

    new commit

--- test.py
+++ test.py
@@ -2,7 +2,9 @@
 
 name="John"
 print ("Hello, " + name + "!")  
+
 print(2**2)
+
 a=2+2 
 
 b=2**2
@@ -10,3 +12,21 @@ b=2**2
 c=10%3
 
 print(a , b  , c )
+print(5 // 2) #result with floating num to 
+print(5 / 2) #jus the decimal part 
+print(2 + 3 * 6) 
+
+print('heyy' + ' dude')
+#print('heyy' + 5 )  #gonna throw error
+
+print('hey! ' * 3)
+# replicatingt the str 
+
+# print('hey' * 'hey') 
+# TypeError: can't multiply sequence by non-int of type 'str'
+
+x = 10
+print(type(x))   # <class 'int'>
+
+print('a' in 'apple') # checks membership
+print ('x' is 'y')  #if these same or not 
