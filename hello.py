4ed568465db1b1fed937c2bafbc9164d1f9c5e62
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-08 10:39:37 +0500

    new commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+
+# dis program gonna great you 
+
+
+print("Hey wassuup!")
+
+print("Wot's ur name...?")
+
+my_name = input('>')
+
+print("Yo! " + my_name + " wot's ur age tho ?")
+
+my_age = input('>')
+
+print("y'll be " + str(int(my_age) + 1) + " in a year!")
