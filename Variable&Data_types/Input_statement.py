d22d8ed8a7b6c5e0657f6b593146f783fa8b6cea
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-09 17:20:34 +0500

    New commit

---
+++ Variable&Data_types/Input_statement.py
@@ -0,0 +1,19 @@
+
+input("Enter your name: ") 
+'''This will always take input as string datatype'''
+
+name = input('>')
+print(name)
+
+age = int(input('>'))
+''' This gonna take input as the integer datatype cz we put type conversion there'''
+print(age)
+
+CGPA = float(input('>'))
+'''This gonna take input as float datatype '''
+print(CGPA)
+
+
+a = input("Any number: ")
+print("You entered " + str(a) + "!")
+print("You entered " , a , "!")
