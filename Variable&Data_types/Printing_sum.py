d2eb7545e97356a8027dc8b2d5b5553269ddb746
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-09 16:52:12 +0500

    New commit

---
+++ Variable&Data_types/Printing_sum.py
@@ -0,0 +1,22 @@
+  
+a = 15 
+b = 10
+sum = a + b
+diff = a - b
+a_power_b = a ** b 
+division = a/b 
+multiply = a*b 
+print("The sum of " + str(a) + " and " + str(b) + " is: " + str(sum))
+print("The sum of", a , "and" , b, "is:" , sum ) 
+
+print("The difference of " + str(a) + " and " + str(b) + " is: " + str(diff))
+print("The difference of", a , "and" , b, "is:" , diff ) 
+
+print("The Product of " + str(a) + " and " + str(b) + " is: " + str(multiply))
+print("The product of", a , "and" , b, "is:" , multiply ) 
+
+print("The division of " + str(a) + " and " + str(b) + " is: " + str(division))
+print("The division of", a , "and" , b, "is:" , division ) 
+
+
+print("a_power_b = a ** b ", "=" , a_power_b) 
---
+++ Variable&Data_types/commenting.py
@@ -0,0 +1,7 @@
+
+#  Hello world 
+""" How's going on 
+                   I am a comment 
+                                m ignoerd """
+
+print("Getting comments ")
