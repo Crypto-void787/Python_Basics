b3ec215dc12139c0265c0963d681356d28795d1d
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-11 10:17:30 +0500

    New commit

---
+++ Chapter_Two/strings.py
@@ -0,0 +1,22 @@
+
+str1 = 'This is str1'
+str2 = "This is str2"
+str3 = """This is str3"""
+
+str4 = "This's using escape sequence. \nLike this n we're in next line."
+
+str5= """This's using tab space.\tNow we've a tab space b/w our sentences!"""
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print(str5)
+
+print("Concatenation of strings be like: " , str1 + " " + str2)
+
+'''Lets's get length of str1 '''
+a = len(str1)
+
+#spaces also gonna include in length 
+print("Length of str1 is : " + str(a)) 
