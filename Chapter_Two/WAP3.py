363921144f27ce7138375303538e590804f3532a
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-14 17:39:24 +0500

    New commit

---
+++ Chapter_Two/WAP3.py
@@ -0,0 +1,9 @@
+# Program to check either given number is even or odd 
+
+num = int(input('Enter the number: '))
+
+if num % 2 == 0 :
+    print('This is even number!')
+
+else :
+    print('This is odd number!')
