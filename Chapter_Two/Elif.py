dc6ca7e657990bcac20d747188d0fd8864aaa961
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-14 17:29:40 +0500

    New commit

--- Chapter_Two/Elif.py
+++ Chapter_Two/Elif.py
@@ -1,3 +1,23 @@
 
 # grading students according to their marks 
 
+Marks = int(input("Enter your marks: "))
+
+if Marks == 90 : 
+    print("Grade: A+")
+
+elif Marks >= 80 and Marks <= 90 :
+    print("Grade: A")
+        
+elif Marks >= 70 and Marks <= 80 :
+    print("Grade: B")
+
+elif Marks >= 50 and Marks <=70 :
+    print("Grade: C")
+
+elif Marks >= 40 and Marks <=50 :
+    print("Grade: D")
+
+else:
+    print("Grade: E")    
+
