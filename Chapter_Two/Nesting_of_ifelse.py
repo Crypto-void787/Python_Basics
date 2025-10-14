7ca75c7eebb14fa8a44b3c648f1434deb4fe3009
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-14 17:35:22 +0500

    New commit

---
+++ Chapter_Two/Nesting_of_ifelse.py
@@ -0,0 +1,15 @@
+
+age = int(input("Enter your age: "))
+
+if age >= 18 :
+
+ #nested loop 
+ 
+    if age > 40 :
+        print("Overage! Can't enroll")
+
+    else:    
+        print("Can enroll")
+
+else :
+    print("Underage! Can't enroll")     
