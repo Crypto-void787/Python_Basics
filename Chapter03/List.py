e378c96e09cd21839cc55be806025e72f90c298a
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-15 12:02:33 +0500

    New commit

---
+++ Chapter03/List.py
@@ -0,0 +1,22 @@
+
+# List are kind of array in any other language 
+''' List used to store multiple variables within jus a single variable 
+     like containers that store multiple values of the diff type like str, int, float in a single variable'''
+
+Marks = [34, 45, 67.6, 56, 98 ]
+
+print(Marks)
+
+print("\nMarks[0]: " + str(Marks[0]))
+print("Marks[1]: " + str(Marks[1]))
+print("Marks[2]: " + str(Marks[2]))
+print("Marks[3]: " + str(Marks[3]))
+print("Marks[4]: " + str(Marks[4]))
+
+print("\n", type(Marks))
+
+Person = ["Saad", 19 ]
+
+print("\n"+Person)
+print("\nName: " + Person[0])
+print("Age: " + str(Person[1]))
