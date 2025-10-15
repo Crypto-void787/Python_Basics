e39dbcda460e555727b4c97fbf5eb58c8f51e998
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-15 12:20:39 +0500

    New commit

--- Chapter03/List.py
+++ Chapter03/List.py
@@ -1,7 +1,7 @@
 
 # List are kind of array in any other language 
 ''' List used to store multiple variables within jus a single variable 
-     like containers that store multiple values of the diff type like str, int, float in a single variable'''
+     but in list in python we can store multiple values of the diff type like str, int, float in a single variable'''
 
 Marks = [34, 45, 67.6, 56, 98 ]
 
@@ -13,10 +13,35 @@ print("Marks[2]: " + str(Marks[2]))
 print("Marks[3]: " + str(Marks[3]))
 print("Marks[4]: " + str(Marks[4]))
 
+print( "Length of List Marks:" , len(Marks))
+
 print("\n", type(Marks))
 
 Person = ["Saad", 19 ]
 
-print("\n"+Person)
+print(Person)
 print("\nName: " + Person[0])
 print("Age: " + str(Person[1]))
+
+print("Length of list Person:" , len(Person))
+
+
+''' Mutable: Which can change 
+     Immutable: which can't change '''
+
+''' In python, Strings are immutable 
+      meanwhile the list are mutable '''
+
+''' As we knw list are mutable. 
+    So using that rule we can change a
+     string in list although its immutable
+      but cz its in list so gonna apply the list rule over it  '''
+
+# For instance
+
+Person[0] = "Ania"
+
+# Now printing that person list 
+# Its altered 
+print(Person)
+
