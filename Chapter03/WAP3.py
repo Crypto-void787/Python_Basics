f8a1be6a4d51c87074e4a6865863e913241a6c59
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-15 18:12:48 +0500

    New commit

---
+++ Chapter03/WAP3.py
@@ -0,0 +1,11 @@
+
+''' Counting the students in the tuple with the grade A'''
+
+Std = ('C', 'D', 'A', 'A', 'B', 'B', 'A', 'C')
+
+print("Students grade list:", Std)
+
+print("Students with grade 'A' :" , Std.count('A') )
+print("Students with grade 'B' :" , Std.count('B') )
+print("Students with grade 'C' :" , Std.count('C') )
+print("Students with grade 'D' :" , Std.count('D') )
---
+++ Chapter03/WAP4.py
@@ -0,0 +1,10 @@
+'''Storing grdes in an order in list  '''
+
+Std = ['C', 'D', 'A', 'A', 'B', 'B', 'A', 'C']
+
+print("Unsorted grade list:" , Std)
+
+Std.sort()
+
+print("\nSorted grade list:" , Std)
+
