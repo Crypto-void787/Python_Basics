f1431854328af50571f7746db699603e7435737a
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-16 09:10:58 +0500

    New commit

---
+++ Chapter04/Nested_Dictionary.py
@@ -0,0 +1,26 @@
+
+''' Let's play with nested dictionary '''
+
+Student = {
+    'Name' : 'Anaya' , 
+    'Grade' : 7 , 
+    'Subjects' : {
+        'Mathametics' : 49.9 ,
+        'German' : 45 
+    } , 
+    'CGPA' : 3.87
+}
+
+print("Student dictionary:\n" , Student)
+print( "\nAccessing nested dictionary named as Subject:\n",Student['Subjects'])
+# print("German" , Student['German'])
+
+
+print( "Name before:", Student['Name'])
+
+Student['Name'] = 'Zoobi' #Altered 
+print( "Name After:", Student['Name'])
+
+Student['Disability'] = None
+
+print("\nStudent dictionary:\n" , Student)
