439992ff3effb826d5adbaa5ba653e75da58a0d0
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-16 11:30:32 +0500

    New commit

---
+++ Chapter04/Set_Methods.py
@@ -0,0 +1,43 @@
+''' like others set also possess methods: 
+       - add() # add you dude `
+       - remove() # remove the given ele 
+       - clear() # empties the set
+       - pop()  #removes random value '''
+
+print("add() Method!")
+A = set()  # an empty set 
+''' Let's add ele in it '''
+print("Before Add(): " + str(A))
+
+A.add(1)
+A.add(2)
+A.add(3)
+A.add(2)
+A.add(4)
+A.add(('T','Y'))
+
+print("After Add(): " + str(A))
+
+
+print("\nremove() Method!")
+print("Before remove(): " + str(A))
+
+A.remove(3)
+
+print("After remove(): " + str(A))
+
+print("\npop() Method!")
+print("Before pop(): " + str(A))
+
+A.pop()
+
+print("After pop(): " + str(A))
+
+print("\nclear() Method!")
+print("Before clear(): " + str(A))
+print("Before clear(): " + str(len(A)))
+
+A.clear()
+
+print("After clear(): " + str(A))
+print("After clear(): " + str(len(A)))
