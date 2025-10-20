140d739bed82e95fe288db557b1b524ff4dce4ae
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-20 12:09:26 +0500

    New commit

---
+++ Chapter06/Function.py
@@ -0,0 +1,26 @@
+# To prevent Redundant code we use functions 
+# block of code that perform specific tasks
+
+''' Function defination: 
+
+ def func_name(param1, param2, ...) : 
+       # same work
+       return val   ''' 
+
+''' Function call: 
+
+funct_name(arg1, arg2 , ...)'''
+
+
+def Sum(a , b) :
+    return a + b
+    # s = a + b
+    # return s 
+
+
+def Sqr(a): 
+   return  (print(a ** 2))
+
+
+print(Sum(4,5))
+Sqr(3)
