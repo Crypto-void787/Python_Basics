3362d4f40ed4719a63da9501318fca1267edf1ab
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-20 13:42:48 +0500

    New commit

--- Chapter06/Function.py
+++ Chapter06/Function.py
@@ -17,10 +17,41 @@ def Sum(a , b) :
     # s = a + b
     # return s 
 
+def Cal_Sum(a , b , c) :
+    s = a + b + c
+    print(s)
+    return s
+    
+
 
 def Sqr(a): 
    return  (print(a ** 2))
 
 
+Cal_Sum(3, 5, 4)
+
 print(Sum(4,5))
 Sqr(3)
+
+''' This function ain't taking any input 
+   n also not returning any output as return vaule '''
+
+def Print_hello(): 
+    print ("Hello")
+
+output = Print_hello()
+print(output) # gonna return none cz no output  
+
+
+''' Let's create a function that calculates the avg of three numbers'''
+def avg(a, b, c) :
+    a = (a + b + c) / 3
+    print("Average is: " + str(a))
+    return a 
+
+avg(1, 2, 3)
+
+
+""" All is need to have proper indentation n we can use aall
+    of the loops n conditional statements right in our functions  """
+
---
+++ Chapter06/Functions_Type.py
@@ -0,0 +1,16 @@
+''' Two types of functions : 
+          1- Built in function 
+           print()
+           len()
+           type()
+           range()
+
+          2- User defined function '''
+
+print("Im built_in function in python")
+
+def Display():
+    print("Im user_defined function XD")
+
+
+Display()    
