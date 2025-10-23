c9fc54ac24f1ebaf260b319cab6da68be517d96e
Wajiha Arif <hackrgirl787@gmail.com>
2025-10-23 11:11:20 +0500

    Format method

---
+++ Chapter02/Format.py
@@ -0,0 +1,19 @@
+''' Use format method '''
+
+name = 'Alice'
+age = 22 
+
+print(f"Hey You {name} , Dude You turned {age} in moments")
+
+print("Today is {} October {}".format("Thursday" , 23))
+
+OSI = 'Open System Interconnection'
+
+print(f'OSI Ref. Model stands for "{OSI} Reference Model" ')
+
+''' Single member testing '''
+s = "GeekforGeeks"
+
+print("Geek" in s )
+print("GFG" in s )
+
